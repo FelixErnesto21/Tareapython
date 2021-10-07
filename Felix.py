c7449faee4799ba51bdb2beb4a4ab748849502b6
FelixErnesto21 <91037114+FelixErnesto21@users.noreply.github.com>
2021-10-07 16:11:45 -0400

    Update Felix.py

--- Felix.py
+++ Felix.py
@@ -8,12 +8,12 @@ def inv_fecha(fec):
     return datetime.strftime(fen,'%Y-%m-%d')
 
 #se hace el calculo del Bono1 y Bono2
-def pago(sueldobase,horas,valor1,valor2):
+def pago(sueldobase,horas,bono1,bono2):
     
     if int(horas) <= 5:
-        calculo= float(sueldobase) + float(sueldobase) / 0.05 * float(valor1)
+        calculo= float(sueldobase) + float(sueldobase) / 0.05 * float(bono1)
     else:
-        calculo= float(sueldobase) + float(sueldobase) / 1.5 * float(valor2)
+        calculo= float(sueldobase) + float(sueldobase) / 1.5 * float(bono2)
     return float(calculo)
 
 #datos a solicitar para calcular los bonos segun horas extras semanales
@@ -21,34 +21,34 @@ bono_general = float(input('Intoduzca bono General:'))
 bono_eficiencia = float(input('Intoduzca bono Eficiencia:'))
 
 
-f = open("pago_nomina_12sep2021.txt", "w")
+f = open("pago_nomina_12sep2021.txt", "w") # creando el archivo de salida con el sueldo a pagar en una archivo .txt
 
 #Leeo el archivo txt
 with open("Archivo nomina.txt", "r") as nomina:
     for linea in nomina:
 
-        campo = linea.split()
+        datos = linea.split()
 
         
-        if campo[0].isdigit(): # Si el dato en la posicion es Verdadero
-            fec = inv_fecha(campo[1]) # Mando a llamar la funcion de inversion de fecha
-            suel = pago(campo[4].replace("$",""),campo[5],bono_general,bono_eficiencia)
+        if datos[0].isdigit(): # Si el dato en la posicion es Verdadero
+            fec = inv_fecha(datos[1]) # Mando a llamar la funcion de inversion de fecha
+            suel = pago(datos[4].replace("$",""),datos[5],bono_general,bono_eficiencia)
         else:
-            fec = campo[1]
-            suel = campo[3]
+            fec = datos[1]
+            suel = datos[3]
 
         
       
-        f.write(campo[0]+" ")
+        f.write(datos[0]+" ")
         f.write(fec+" ")
-        f.write(campo[2]+" ")
-        f.write(campo[3]+" ")
-        f.write(campo[4]+" ")
+        f.write(datos[2]+" ")
+        f.write(datos[3]+" ")
+        f.write(datos[4]+" ")
         
 
        
-        if campo[0].isdigit():
-            f.write(campo[5]+" ")
+        if datos[0].isdigit():
+            f.write(datos[5]+" ")
             f.write(str(suel))
             f.write(os.linesep)
         else:
@@ -56,5 +56,5 @@ with open("Archivo nomina.txt", "r") as nomina:
            
         
 
-f.close()
+f.close() # Cerrando el archivo creado
 
